8931ab2b59bb6b4821106a2300942509cccdaa03
OteIan <oteian9@gmail.com>
2024-03-15 14:27:01 +0300

    Make multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" functions """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ Make multiplier """
+    return lambda x: x* multiplier
