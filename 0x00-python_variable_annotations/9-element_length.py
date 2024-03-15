2896bdb0428e8ca24b832fee845059b1ccb42bbd
OteIan <oteian9@gmail.com>
2024-03-15 14:36:07 +0300

    Fix: Documentation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""  """
+""" Duck types """
 from typing import Iterable, List, Tuple, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """  """
+    """ Duck types """
     return [(i, len(i)) for i in lst]
