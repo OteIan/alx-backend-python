d1cde6b062d1990cf2790a6512220e65a2476468
OteIan <oteian9@gmail.com>
2024-03-18 22:23:26 +0300

    Basic async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" .. """
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Random """
+    wait_seconds: float = random.random() * max_delay
+    await asyncio.sleep(wait_seconds)
+    return wait_seconds
+    
