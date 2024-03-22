faa55dd6aae9a1f2478292f47a590042e3c4153c
OteIan <oteian9@gmail.com>
2024-03-22 22:44:58 +0300

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Async Generator
+"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """
+    Yields a random number btwn 1 & 10
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        time: float = random.random() * 10
+        yield time
