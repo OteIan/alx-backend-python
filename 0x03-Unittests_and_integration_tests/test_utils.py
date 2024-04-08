ae2315edcd0cc361dc692d37b33317ea8d8db5a2
Rich_ar_Dev <moriasrichar3@gmail.com>
2024-04-08 09:29:34 +0300

    Task 2: Exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,14 @@ Test module for utils module
 import unittest
 import utils
 from parameterized import parameterized
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+    Union
+)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -16,6 +24,25 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, result):
+    def test_access_nested_map(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            result: Union[Dict, int]
+            ) -> None:
         """Test utils.access_nested_map"""
         self.assertEqual(utils.access_nested_map(nested_map, path), result)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            err: Exception
+            ) -> None:
+        """ Test for exceptions """
+        with self.assertRaises(err):
+            utils.access_nested_map(nested_map, path)
